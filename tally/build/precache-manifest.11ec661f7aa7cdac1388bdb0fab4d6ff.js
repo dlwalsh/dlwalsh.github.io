5a7c19bd9ceeaf89e3f18bec48048521bab23012
David Walsh <dlwalsh@gmail.com>
2019-02-24 15:59:24 +1100

    tally

---
+++ tally/build/precache-manifest.11ec661f7aa7cdac1388bdb0fab4d6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02dd6a0f18527ad2926e",
+    "url": "/static/css/main.cf322524.chunk.css"
+  },
+  {
+    "revision": "02dd6a0f18527ad2926e",
+    "url": "/static/js/main.02dd6a0f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d4f5996a94dd0737be51",
+    "url": "/static/js/2.d4f5996a.chunk.js"
+  },
+  {
+    "revision": "c9ad439ca368457a642f8f7140268aee",
+    "url": "/index.html"
+  }
+];
